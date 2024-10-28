7eb497f6b6c582d9a9dd7732437b1e8b1581d0af
SergioAndres17 <saordonez-2023a@corhuila.edu.co>
2024-10-28 08:56:45 -0500

    Delete caso2.sql

--- caso2.sql
+++
@@ -1,7 +0,0 @@
-SELECT v.name AS view_name
-FROM role r
-JOIN role_module rm ON r.id = rm.role_id
-JOIN module m ON rm.module_id = m.id
-JOIN module_view mv ON m.id = mv.module_id
-JOIN view v ON mv.view_id = v.id
-WHERE r.name = "Admin";
